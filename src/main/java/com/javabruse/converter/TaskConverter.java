acd3c7ef8a1a30ba51d6035431837f631dbac5e4
FiataliS <FiataliS@gmail.com>
2025-09-27 13:45:37 +0500

    update cam message to string

--- src/main/java/com/javabruse/converter/TaskConverter.java
+++ src/main/java/com/javabruse/converter/TaskConverter.java
@@ -1,4 +1,39 @@
 package com.javabruse.converter;
 
+import com.javabruse.DTO.TaskRequest;
+import com.javabruse.DTO.TaskResponse;
+import com.javabruse.model.Task;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class TaskConverter {
+
+    private final PhotoConverter photoConverter;
+
+    public TaskResponse taskToTaskResponse(Task task){
+        TaskResponse taskResponse = new TaskResponse();
+        taskResponse.setId(task.getId());
+        taskResponse.setName(task.getName());
+        taskResponse.setStatus(task.getStatus());
+        taskResponse.setCreatedAt(task.getCreatedAt());
+        taskResponse.setUpdatedAt(task.getUpdatedAt());
+        return taskResponse;
+    }
+
+    public Task taskRequestNew(TaskRequest taskRequest, UUID userId){
+        Task task = new Task();
+        task.setName(taskRequest.getName());
+        task.setUserId(userId);
+        task.setStatus(taskRequest.getStatus());
+        task.setPhotos(null);
+        return task;
+    }
+
+    public Task taskRequestUpdate(TaskRequest taskRequest, Task taskOld){
+        taskOld.setName(taskRequest.getName());
+        taskOld.setStatus(taskRequest.getStatus());
+        return taskOld;
+    }
 }
