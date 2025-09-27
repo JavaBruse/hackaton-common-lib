bddd1b3d27896a7402d63ca12ed27a966ab1a742
FiataliS <FiataliS@gmail.com>
2025-09-27 09:30:15 +0500

    update user id to task

--- src/main/java/com/javabruse/repository/TaskRepo.java
+++ src/main/java/com/javabruse/repository/TaskRepo.java
@@ -4,8 +4,13 @@ import com.javabruse.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepo  extends JpaRepository<Task, UUID> {
+    Optional<Task> findByIdAndUserId(UUID id, UUID userId);
+    List<Task> findByUserid(UUID userId);
+
 }
