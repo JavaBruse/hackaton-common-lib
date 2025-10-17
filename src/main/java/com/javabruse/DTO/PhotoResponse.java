21f19dc575b5d9ab433c71c55eb6afd36a83e947
FiataliS <FiataliS@gmail.com>
2025-10-17 13:05:55 +0500

    delete all update code

--- src/main/java/com/javabruse/DTO/PhotoResponse.java
+++ src/main/java/com/javabruse/DTO/PhotoResponse.java
@@ -3,7 +3,6 @@ package com.javabruse.DTO;
 import com.javabruse.model.Status;
 import lombok.Getter;
 import lombok.Setter;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -12,28 +11,16 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@Schema(description = "Модель ответа для фотографии")
 public class PhotoResponse {
-    @Schema(description = "UUID фотографии", example = "123e4567-e89b-12d3-a456-426614174001")
     private UUID id;
-    @Schema(description = "Путь к оригинальному файлу", example = "http://URL_S3/photos/original.jpg")
     private String filePathOriginal;
-    @Schema(description = "Путь к обработанному файлу", example = "http://URL_S3/photos/original.jpg")
     private String filePathComplete;
-    @Schema(description = "Хэш файла", example = "a1b2c3d4e5f6")
     private String fileHash;
-    @Schema(description = "Оригинальне имя фото", example = "photo123.jpg")
     private String name;
-    @Schema(description = "MIME-тип файла", example = "image/jpeg")
     private String contentType;
-    @Schema(description = "Размер файла в байтах", example = "2048576")
     private Long fileSize;
-    @Schema(description = "Статус обработки", example = "COMPLETED")
     private Status status;
-    @Schema(description = "Время обновления в миллисекундах", example = "1700000000000")
     private Long updatedAt = Instant.now().toEpochMilli();
-    @Schema(description = "Метаданные из фотографии, или далее определенные моделью")
     private CamMetadataResponse camMetadataResponse;
-    @Schema(description = "Метаданные сооружений на фото")
     private List<ConstructMetadataResponse> constructMetadataResponses = new ArrayList<>();
 }
